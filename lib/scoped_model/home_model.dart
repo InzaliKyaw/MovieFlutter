3d2ab2169fce7b31ac5b4741c4f3c9e6c294a262
Zoey Kyaw <zoey.k@sdbank.co>
2024-05-03 10:33:01 +1000

    home model update

---
+++ lib/scoped_model/home_model.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:scoped_model/scoped_model.dart';
+import 'package:the_movie_app_padc/data/models/movie_booking_model.dart';
+import 'package:the_movie_app_padc/data/vos/movie_vo.dart';
+import 'package:the_movie_app_padc/utils/strings.dart';
+
+class HomeModel extends Model{
+
+  /// Model
+  final MovieBookingModel _model = MovieBookingModel();
+
+  /// Now showing or Coming Soon
+  String selectedText = kNowShowingLabel;
+
+  /// Now Playing Movies
+  List<MovieVO> nowPlayingMovies = [];
+
+  /// Coming Soon Movies
+  List<MovieVO> comingSoonMovies = [];
+
+
+  /// Coming Soon Movies
+  List<MovieVO> comingSoonMoviesFromDatabase = [];
+
+  List<MovieVO> nowPlayingMoviesFromDatabase = [];
+
+  /// Movies To Show
+  List<MovieVO> moviesToShow = [];
+
+  StreamSubscription? _nowPlayingMoviesSubscription;
+  StreamSubscription? _comingSoonMoviesSubscription;
+
+  HomeModel(){
+    /// Now Playing Movies From Database
+    // _nowPlayingMoviesSubscription = _model.getNowPlayingMoviesFromDatabase();
+    // .listen((nowPlayingMoviesFromDatabase);
+    nowPlayingMovies = nowPlayingMoviesFromDatabase;
+    if(moviesToShow.isEmpty){
+      moviesToShow = nowPlayingMoviesFromDatabase;
+      notifyListeners();
+    }
+
+    /// Coming Soon Movies From Database
+    // _comingSoonMoviesSubscription = _model.getComingSoonMoviesFromDatabase();
+    comingSoonMovies = comingSoonMoviesFromDatabase;
+
+    /// Now Playing Movies From Network
+    _model.getNowPlayingMovies();
+
+    /// Coming Soon Movies From Network
+    _model.getComingSoonMovies();
+  }
+
+  void onTapNowShowingOrComingSoon(String text){
+    /// Set Now Playing or Coming Soon
+    selectedText = text;
+
+    /// Set Movies
+    if( text == kNowShowingLabel){
+      moviesToShow = nowPlayingMovies;
+    }else{
+      moviesToShow = comingSoonMovies;
+    }
+    notifyListeners();
+  }
+
+  void onDispose(){
+    _nowPlayingMoviesSubscription?.cancel();
+    _comingSoonMoviesSubscription?.cancel();
+  }
+
+}
